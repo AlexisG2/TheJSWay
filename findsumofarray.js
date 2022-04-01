03055faae117d3a2ffda1e709d8e46eb5cb660d8
Your name here <GordilloAlexisCTE@gmail.com>
2022-03-31 20:21:49 -0400

    push

---
+++ findsumofarray.js
@@ -0,0 +1,8 @@
+/* Sum of values
+Write a program that creates the following array, then calculates and shows the sum of its values (42 in that case). const values = [3, 11, 7, 2, 9, 10]; */
+
+const values = [3, 11, 7, 2, 9, 10];
+let sum = 0
+values.forEach(num => {
+    sum += num})
+console.log(sum)

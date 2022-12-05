f12944e2d6f4a83187fb92a9d7ed14f59e424acd
Your name here <GordilloAlexisCTE@gmail.com>
2022-12-05 17:04:37 -0500

    Daily push

---
+++ minoftwonums.js
@@ -0,0 +1,11 @@
+//Minimum of two numbers
+//Let's pretend the JavaScript Math.min() function doesn't exist. Complete the 
+//following program so that the min() function returns the minimum of its two received numbers.
+
+function min(x,y){
+    if(x < y){
+        console.log(x)
+    }else{
+        console.log(y)
+    }
+}
